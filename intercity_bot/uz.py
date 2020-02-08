5e4c0f2b088915a83b3743fe7792ee7ba08f2b58
Mayor <mayor.off.dev@gmail.com>
2020-02-08 03:26:17 +0200

    add stations search

---
+++ intercity_bot/uz.py
@@ -0,0 +1,29 @@
+import aiohttp
+import json
+from urllib.parse import urlencode
+
+API_URL = "https://booking.uz.gov.ua/"
+
+
+
+class Station:
+    def __init__(self, title, id):
+        self.title = title
+        self.id = id
+
+
+async def api_request(method: str, parameters: dict, type: str, lang: str = "ru"):
+    async with aiohttp.ClientSession() as session:
+        if type == 'get':
+            url = "{}{}/{}?{}".format(API_URL, lang, method, urlencode(parameters))
+            async with session.get(url) as response:
+                return json.loads(await response.text())
+        elif type == 'post':
+            url = "{}{}/{}".format(API_URL, lang, method)
+            async with session.post(url) as response:
+                return json.loads(await response.text())
+
+
+async def search_stations(search_request: str):
+    result = await api_request('train_search/station/', {'term': search_request}, 'get')
+    return map(lambda station: Station(station['title'], station['value']), result)
