670c14f9a136148ea219fa7770c5c159356ded4d
Mayor <mayor.off.dev@gmail.com>
2020-02-10 11:14:31 +0200

    add carriage functions

--- intercity_bot/uz.py
+++ intercity_bot/uz.py
@@ -76,8 +76,47 @@ async def search_trains(user: User, station_from_id: int, station_to_id: int, da
         else:
             raise UnknownException()
 
-    return map(lambda t: Train(t['num'], t['category'], t['travelTime'], 
-                               t['from'], t['to']), result['data']['list'])
+    return list(map(
+        lambda t: Train(
+            t['num'], t['category'], t['travelTime'], 
+            t['from'], t['to']
+        ), 
+        result['data']['list']
+    ))
+
+
+async def get_train_carriages(user: User, station_from_id: int, station_to_id: int, date: str, train_num: str, carriage_type: str = None, captcha: str = None):
+    parameters = {'from': station_from_id, 'to': station_to_id, 'date': date, 'train': train_num}
+    if type(carriage_type) is str:
+        parameters['wagon_type_id'] = carriage_type
+    if type(captcha) is str:
+        parameters['captcha'] = captcha
+
+    result = await api_request('train_wagons/', parameters, 'post-urlencode', user)
+
+    if 'error' in result:
+        if result['error'] == 1:
+            raise CaptchaException()
+        else:
+            raise UnknownException()
+
+    return result
+
+
+async def get_carriage(user: User, station_from_id: int, station_to_id: int, date: str, train_num: str, carriage_num: int, carriage_type: str, carriage_class: int, captcha: str = None):
+    parameters = {'from': station_from_id, 'to': station_to_id, 'date': date, 'train': train_num, 'wagon_num': carriage_num, 'wagon_type': carriage_type, 'wagon_class': carriage_class}
+    if type(captcha) is str:
+        parameters['captcha'] = captcha
+
+    result = await api_request('train_wagon/', parameters, 'post-urlencode', user)
+
+    if 'error' in result:
+        if 'captcha' in result:
+            raise CaptchaException()
+        else:
+            raise UnknownException()
+
+    return result
 
 
 async def get_captcha(user: User):
